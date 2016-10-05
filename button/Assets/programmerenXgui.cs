e79e2ef6220aaf33eb633e1f31d254aa0612d9fc
gmd1a-carmenbakker <carmenbakker@hotmail.com>
2016-10-05 13:53:57 +0200

    button unity

---
+++ button/Assets/programmerenXgui.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class programmerenXgui : MonoBehaviour {
+
+    public Button knop;
+    public Text tekst;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+	
+    public void buttonClicked()
+    {
+        tekst.text = "geklikt";
+    }
+	
+}
+
+   
